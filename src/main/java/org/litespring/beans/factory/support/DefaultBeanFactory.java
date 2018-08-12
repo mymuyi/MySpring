4589483e6df854ce544257f43dd544955556bd4a
muyi <13257329926@163.com>
2018-08-12 16:18:30 +0800

    重构，提取BeanDefinitonRegistry接口

--- src/main/java/org/litespring/beans/factory/support/DefaultBeanFactory.java
+++ src/main/java/org/litespring/beans/factory/support/DefaultBeanFactory.java
@@ -1,67 +1,18 @@
 package org.litespring.beans.factory.support;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.Element;
-import org.dom4j.io.SAXReader;
 import org.litespring.beans.BeanDefinition;
 import org.litespring.beans.factory.BeanCreationException;
-import org.litespring.beans.factory.BeanDefinitionStoreException;
 import org.litespring.beans.factory.BeanFactory;
 import org.litespring.utils.ClassUtils;
 
-public class DefaultBeanFactory implements BeanFactory {
-
-	public static final String ID_ATTRIBUTE = "id";
-
-	public static final String CLASS_ATTRIBUTE = "class";
+public class DefaultBeanFactory implements BeanFactory, BeanDefinitionRegistry{
 
 	private final Map<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<>(64);
 
-	public DefaultBeanFactory(String configFile) {
-		loadBeanDefinition(configFile);
-	}
-
-	private void loadBeanDefinition(String configFile) {
-		InputStream is = null;
-		try {
-			ClassLoader cl = ClassUtils.getDefaultClassLoader();
-			is = cl.getResourceAsStream(configFile);
-
-			SAXReader reader = new SAXReader();
-			Document doc = reader.read(is);
-
-			Element root = doc.getRootElement();
-			Iterator<Element> it = root.elementIterator();
-
-			while (it.hasNext()) {
-				Element ele = it.next();
-
-				String id = ele.attributeValue(ID_ATTRIBUTE);
-				String beanClassName = ele.attributeValue(CLASS_ATTRIBUTE);
-
-				BeanDefinition bd = new GenericBeanDefinition(id, beanClassName);
-
-				this.beanDefinitionMap.put(id, bd);
-			}
-
-		} catch (DocumentException e) {
-			throw new BeanDefinitionStoreException("IOException parsing XML document from " + configFile,e);
-		} finally {
-			if (is != null) {
-				try {
-					is.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
+	public DefaultBeanFactory() {
 	}
 
 	public BeanDefinition getBeanDefinition(String beanID) {
@@ -84,6 +35,11 @@ public class DefaultBeanFactory implements BeanFactory {
 		} catch (Exception e) {
 			throw new BeanCreationException("create bean for "+ beanClassName +" failed",e);
 		}
+	}
+
+	@Override
+	public void registerBeanDefinition(String beanID, BeanDefinition bd) {
+		this.beanDefinitionMap.put(beanID, bd);
 	}
 
 }
--- src/test/java/org/litespring/test/v1/BeanFactoryTest.java
+++ src/test/java/org/litespring/test/v1/BeanFactoryTest.java
@@ -4,49 +4,66 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.litespring.beans.BeanDefinition;
 import org.litespring.beans.factory.BeanCreationException;
 import org.litespring.beans.factory.BeanDefinitionStoreException;
-import org.litespring.beans.factory.BeanFactory;
 import org.litespring.beans.factory.support.DefaultBeanFactory;
+import org.litespring.beans.factory.xml.XmlBeanDefinitionReader;
 import org.litespring.service.v1.PetStoreService;
 
 public class BeanFactoryTest {
+	
+	DefaultBeanFactory factory = null;
+
+	XmlBeanDefinitionReader reader = null;
+	
+	@Before
+	public void setUp() {
+		
+		factory = new DefaultBeanFactory();
+
+		reader = new XmlBeanDefinitionReader(factory);
+		
+	}
 
 	@Test
 	public void testGetBean() {
-		
-		BeanFactory factory = new DefaultBeanFactory("petstore-v1.xml");
-		
+
+		reader.loadBeanDefinition("petstore-v1.xml");
+
 		BeanDefinition bd = factory.getBeanDefinition("petStore");
-		
+
 		assertEquals("org.litespring.service.v1.PetStoreService", bd.getBeanClassName());
-		
-		PetStoreService petStore = (PetStoreService)factory.getBean("petStore");
-		
+
+		PetStoreService petStore = (PetStoreService) factory.getBean("petStore");
+
 		assertNotNull(petStore);
 	}
-	
+
 	@Test
 	public void testInvalidBean() {
-		BeanFactory factory = new DefaultBeanFactory("petstore-v1.xml");
+
+		reader.loadBeanDefinition("petstore-v1.xml");
 		try {
 			factory.getBean("invalidBean");
-		} catch(BeanCreationException e) {
+		} catch (BeanCreationException e) {
 			return;
 		}
 		Assert.fail("expect BeanCreationException");
 	}
-	
+
 	@Test
 	public void testInvalidXML() {
 		try {
-			BeanFactory factory = new DefaultBeanFactory("xxx.xml");
+
+			reader.loadBeanDefinition("xxx.xml");
+			
 		} catch (BeanDefinitionStoreException e) {
 			return;
 		}
-		
+
 		Assert.fail("expect BeanDefinitionStoreException");
 	}
 }
